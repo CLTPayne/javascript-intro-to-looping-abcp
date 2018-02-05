c0cab1a9893113e5010c9dd0a3a6eb87daaa09fc
Charlotte Payne <cltpayne@gmail.com>
2018-02-05 14:29:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-forLoop (Array) {
+function forLoop (Array) {
   for (let i = 0; i < 25; i++) {
     if (i = 1) {console.log( "I am ${i} strange loop")}
     else {console.log( "I am ${i} strange loops")}
