a46b1e04b572c59fd3d7fe055df2fadaafc6e47f
Charlotte Payne <cltpayne@gmail.com>
2018-02-05 14:54:37 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -9,6 +9,12 @@ function forLoop(array) {
   return array
 }
 
+function whileLoop(n) {
+  while (n > 0) {
+  console.log(--n)
+  }
+  return 
+}
 function whileLoop(number) {
   let countdown = number;
   while (countdown > 0) {
